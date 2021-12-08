67f886313a7e9064da8295746f0cd0cceb2c2296
GolfDvojka92 <71879490+GolfDvojka92@users.noreply.github.com>
2021-12-09 00:03:20 +0100

    DNK Lanac

---
+++ DNK Lanac/zad8.java
@@ -0,0 +1,17 @@
+public class zad8 {
+    public static void main(String[] args){
+        DNKLanac dnk = new DNKLanac(8);
+
+        dnk.dodaj('A');
+        dnk.dodaj('C');
+        dnk.dodaj('C');
+        dnk.dodaj('G');
+        dnk.dodaj('T');
+        dnk.dodaj('T');
+        dnk.dodaj('T');
+        dnk.dodaj('T');
+
+        dnk.ispisiKarike();
+        dnk.ispisiKarikeObrnuto();
+    }
+}
